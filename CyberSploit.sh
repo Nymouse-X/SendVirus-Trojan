23a6db95e49252ed7024bd1cb25b995db5d20c85
NymouseX <saibaougokasu@gmail.com>
2021-11-03 22:51:59 +0700

    senVirus&Trojan

---
+++ CyberSploit.sh
@@ -0,0 +1,674 @@
+# buat password
+clear
+echo "Login Dulu Soalnya Bahaya" | lolcat
+sleep 1
+echo "====================================================="|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "====================================================="|lolcat
+qu="Welcome To Cyber World Hanter Exploit" 
+	read -p "Masukan Nama Anda :" NM ;
+	read -p "Masukan Password :" pass ;
+	sleep 0.1
+echo
+echo $qu | lolcat
+echo "you also get a special emblem" | lolcat
+	sleep 0.1
+#kondisi
+if [ $pass = "CyberSploit" ]
+	then 
+	echo 
+	echo "Password Sucses" | lolcat
+	sleep 0.1
+	else
+	echo "Password Falid" | lolcat
+	echo "Cek Youtube NymouseX" | lolcat
+	sleep 3
+	bash CyberSploit.sh
+	sleep 0.1
+fi
+#tampilan
+clear
+echo "C̸̜̦͈̈̀͒̆̐̊̊̿̚y̷̨̖͓̬̤̫̼͍̳͇̬̪̰̥̰̐̒͒̋̇̈́̃̈́̇̾̕b̸̨̡̩̤͙͖̮̂̀̂̒̋̕͜͜e̸͎̟̱̭͙̿̔̚͜r̴̢̧̨̤̱̥̟̮̗̮̫̱̤͌̀͌̑͗̈́̂͗̓͝͠ ̵̡̙̝͇̳̗̭̜͉̼̗͆́̈́H̷̜̯̮̮̼͕̹͎̝̦͖͖͚̺̆́̔̐a̵̺̖̾̽̎͗͊̃͐̒̓̌͐͆͠͝ṅ̵̛̖̞̔́͝t̸̗͑͑̔͗̚̚ͅe̶͈̖̠̦̩̍͌̅̈́̓͘r̶̢̻̦̝̘̜̣͋͑͑͛̌̀̕̕̚̚ ̷͈̦̋̊͂̀̽̊͆̿́̍̈́͆̕͝E̴̖̮͕̜̦͓̅͂́́x̶̳̘͎̻̙̗͔͔̯̹̂̏̋͑̌̓̈́̐̃͜p̵̢̲̞̩̣̣͇̂͆͒̎̒͑̇̒̽̚͜͝ͅl̷̨̰̯̎́ǫ̵̧̟͓̐i̷̖̜̥͕̓̉̂̓͗̋͂ẗ̵̢̳͔̲̼͚̤̳̼́̒́̓͑̃́̏̉̀" | lolcat
+echo
+echo "Seting Tampilan" | lolcat
+read -p "User :" user ;
+read -p "Hacking Name :" name ;
+
+echo "Selamat Kamu Sudah Resmi Menjadi Anggota" | lolcat
+sleep 1
+#anggota
+clear
+echo "Welcome To Cyber Sploit" | lolcat
+sleep 1
+toilet -f big -F gay $name
+echo "====================================================="|lolcat
+echo "User   : $user"|lolcat
+echo "My Team : Cyber Hanter Exploit"|lolcat
+echo "Hack By : $name" | lolcat
+echo "====================================================="|lolcat
+echo "Teknik Haking" | lolcat
+echo
+echo "1.virus" | lolcat
+echo "2.Trojan" | lolcat
+echo
+read -p "꧁༺$name༻꧂ :" xyz
+#virus
+if [ $xyz = 1 ] || [ $xyz = 1 ]
+then
+clear
+echo "Memulai Proses"
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "Bentar Cek Bahan Dulu"
+pkg remove -y game-repo
+clear
+pkg remove -y science-repo
+clear
+apt insttall -y dnsuntil
+clear
+apt-get install -y dig
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "
+────────────────────────────────────────────────────────────────────────────
+─██████──██████─██████████─████████████████───██████──██████─██████████████─
+─██░░██──██░░██─██░░░░░░██─██░░░░░░░░░░░░██───██░░██──██░░██─██░░░░░░░░░░██─
+─██░░██──██░░██─████░░████─██░░████████░░██───██░░██──██░░██─██░░██████████─
+─██░░██──██░░██───██░░██───██░░██────██░░██───██░░██──██░░██─██░░██─────────
+─██░░██──██░░██───██░░██───██░░████████░░██───██░░██──██░░██─██░░██████████─
+─██░░██──██░░██───██░░██───██░░░░░░░░░░░░██───██░░██──██░░██─██░░░░░░░░░░██─
+─██░░██──██░░██───██░░██───██░░██████░░████───██░░██──██░░██─██████████░░██─
+─██░░░░██░░░░██───██░░██───██░░██──██░░██─────██░░██──██░░██─────────██░░██─
+─████░░░░░░████─████░░████─██░░██──██░░██████─██░░██████░░██─██████████░░██─
+───████░░████───██░░░░░░██─██░░██──██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+─────██████─────██████████─██████──██████████─██████████████─██████████████─
+────────────────────────────────────────────────────────────────────────────" | lolcat
+sleep 1
+echo "Isi Target Kamu" | lolcat
+echo "Nomor/Ip/Link" | lolcat
+echo
+read -p "꧁༺$name༻꧂ :" IP
+dig $IP
+sleep 5
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+clear
+echo "
+███╗░░░███╗██╗░██████╗░██████╗██╗░█████╗░███╗░░██╗
+████╗░████║██║██╔════╝██╔════╝██║██╔══██╗████╗░██║
+██╔████╔██║██║╚█████╗░╚█████╗░██║██║░░██║██╔██╗██║
+██║╚██╔╝██║██║░╚═══██╗░╚═══██╗██║██║░░██║██║╚████║
+██║░╚═╝░██║██║██████╔╝██████╔╝██║╚█████╔╝██║░╚███║
+╚═╝░░░░░╚═╝╚═╝╚═════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+
+░█████╗░░█████╗░███╗░░░███╗██████╗░██╗░░░░░███████╗████████╗███████╗  ░░░░░░
+██╔══██╗██╔══██╗████╗░████║██╔══██╗██║░░░░░██╔════╝╚══██╔══╝██╔════╝  ░░░░░░
+██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██║░░░░░█████╗░░░░░██║░░░█████╗░░  ░░░░░░
+██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██║░░░░░██╔══╝░░░░░██║░░░██╔══╝░░  ░░░░░░
+╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░███████╗███████╗░░░██║░░░███████╗  ██╗██╗
+░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝╚══════╝░░░╚═╝░░░╚══════╝  ╚═╝╚═╝" | lolcat
+fi
+#trojan
+if [ $xyz = 2 ] || [ $xyz = 2 ]
+then
+clear
+echo "Memulai Proses"
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "Bentar Cek Bahan Dulu"
+pkg remove -y game-repo
+clear
+pkg remove -y science-repo
+clear
+apt insttall -y dnsuntil
+clear
+apt-get install -y dig
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "
+───────────────────────────────────────────────────────────────────────────────────────────────────────
+─██████████████─████████████████───██████████████─────────██████─██████████████─██████──────────██████─
+─██░░░░░░░░░░██─██░░░░░░░░░░░░██───██░░░░░░░░░░██─────────██░░██─██░░░░░░░░░░██─██░░██████████──██░░██─
+─██████░░██████─██░░████████░░██───██░░██████░░██─────────██░░██─██░░██████░░██─██░░░░░░░░░░██──██░░██─
+─────██░░██─────██░░██────██░░██───██░░██──██░░██─────────██░░██─██░░██──██░░██─██░░██████░░██──██░░██─
+─────██░░██─────██░░████████░░██───██░░██──██░░██─────────██░░██─██░░██████░░██─██░░██──██░░██──██░░██─
+─────██░░██─────██░░░░░░░░░░░░██───██░░██──██░░██─────────██░░██─██░░░░░░░░░░██─██░░██──██░░██──██░░██─
+─────██░░██─────██░░██████░░████───██░░██──██░░██─██████──██░░██─██░░██████░░██─██░░██──██░░██──██░░██─
+─────██░░██─────██░░██──██░░██─────██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─██░░██──██░░██████░░██─
+─────██░░██─────██░░██──██░░██████─██░░██████░░██─██░░██████░░██─██░░██──██░░██─██░░██──██░░░░░░░░░░██─
+─────██░░██─────██░░██──██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██──██░░██─██░░██──██████████░░██─
+─────██████─────██████──██████████─██████████████─██████████████─██████──██████─██████──────────██████─
+───────────────────────────────────────────────────────────────────────────────────────────────────────" | lolcat
+sleep 1
+echo "Isi Target Kamu" | lolcat
+echo "Nomor/Ip/Link" | lolcat
+echo
+read -p "꧁༺$name༻꧂ :" IP
+traceroute $IP
+dig $IP
+sleep 5
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+echo "Virus Sedang di kirim Ke $IP"
+sleep 0.1
+clear
+echo "
+███╗░░░███╗██╗░██████╗░██████╗██╗░█████╗░███╗░░██╗
+████╗░████║██║██╔════╝██╔════╝██║██╔══██╗████╗░██║
+██╔████╔██║██║╚█████╗░╚█████╗░██║██║░░██║██╔██╗██║
+██║╚██╔╝██║██║░╚═══██╗░╚═══██╗██║██║░░██║██║╚████║
+██║░╚═╝░██║██║██████╔╝██████╔╝██║╚█████╔╝██║░╚███║
+╚═╝░░░░░╚═╝╚═╝╚═════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+
+░█████╗░░█████╗░███╗░░░███╗██████╗░██╗░░░░░███████╗████████╗███████╗  ░░░░░░
+██╔══██╗██╔══██╗████╗░████║██╔══██╗██║░░░░░██╔════╝╚══██╔══╝██╔════╝  ░░░░░░
+██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██║░░░░░█████╗░░░░░██║░░░█████╗░░  ░░░░░░
+██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██║░░░░░██╔══╝░░░░░██║░░░██╔══╝░░  ░░░░░░
+╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░███████╗███████╗░░░██║░░░███████╗  ██╗██╗
+░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝╚══════╝░░░╚═╝░░░╚══════╝  ╚═╝╚═╝" | lolcat
+fi
+
+
+
+
+
